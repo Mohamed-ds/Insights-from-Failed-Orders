5a6f09f69fd68fe9244ba336298c2c2097a5aa4e
Mohamed-ds <68775256+Mohamed-ds@users.noreply.github.com>
2022-12-01 13:32:25 +0200

    update my opinion

--- orders.py
+++ orders.py
@@ -33,6 +33,11 @@ dataset = dataset.rename(columns={
 #    cancellations before and after driver assignment, 
 #    and reasons for order rejection. Analyse the resulting plot. 
 #    Which category has the highest number of orders?
+# My opinion:
+# There are 13435 orders canceled by the client, and 9469 rejected by the system.
+# The high number of orders canceled before a driver is assigned implies that maybe customers have waited too long and have decided to choose an alternative for their transportation needs. 
+# There are 8360 client cancellations after a driver has been assigned, and only four were rejected by the system.
+
 # =============================================================================
 
 A1 = dataset.pivot_table(columns=["is_driver_assigned", "order_status"], values="order_gk", aggfunc="count")
@@ -45,6 +50,12 @@ p1 = A1.plot(kind="bar", subplots=False, figsize=(7, 7), legend=True, rot=0)
 # Plot the distribution of failed orders by hours. 
 # Is there a trend that certain hours have an abnormally high proportion of one category or another? 
 # What hours are the biggest fails? How can this be explained?
+
+# My opinion:
+# For the four offers rejected by the system, those were at 12 midnight. 
+# The rest of the categories follow a similar distribution.
+# the biggest fail hours are at 8 AM.
+
 # =============================================================================
 
 plt.figure()
@@ -73,28 +84,32 @@ p2 = A2.reset_index().pivot(index="hours",
 # Question 3
 # Plot the average time to cancellation with and without driver, by hour. 
 # Can we draw any conclusions from this plot?
+
+# My opinion:
+# The average time to cancellation is always higher on orders with an assigned driver than without. 
+# At 3 A.M. the average time to cancellation arrive at the top, At this time there are a lot of client cancellations.
+
 # =============================================================================
 
 
 plt.figure() 
 A3 = dataset.groupby(by=["hours", "is_driver_assigned"])["cancellations_time_in_seconds"].mean()
-p3 = A3.reset_index().pivot(index="hours",
-                                   columns="is_driver_assigned",
-                                   values="cancellations_time_in_seconds").plot(xticks=range(0, 24),
-                                                                                figsize=(13, 7),
-                                                                                title="Average Time to Cancellation Per Hour and Driver Assignment")
+p3 = A3.reset_index().pivot(index="hours",columns="is_driver_assigned",values="cancellations_time_in_seconds").plot(xticks=range(0, 24),figsize=(13, 7),title="Average Time to Cancellation Per Hour and Driver Assignment")
 
 
 # =============================================================================
 # Question 4
 # Plot the distribution of average ETA by hours. How can this plot be explained?
+
+
+# My opinion:
+# The number of failed orders increases as the average waiting time of the client increases.
+
 # =============================================================================
 
 plt.figure()
 A4 = dataset.groupby(by="hours")["m_order_eta"].mean()
-p4 = A4.plot(figsize=(14, 7),
-                                                           xticks=range(0, 24),
-                                                           title="Average ETA per hour")
+p4 = A4.plot(figsize=(14, 7),xticks=range(0, 24),title="Average ETA per hour")
 
 
 
